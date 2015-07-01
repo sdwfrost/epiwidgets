971c0343daaa7934cab122bca22438aa0d6c79e8
sdwfrost <sdwfrost@gmail.com>
2015-07-01 16:49:03 +0100

    Playing with ggplot colors

--- inst/htmlwidgets/packwidget.js
+++ inst/htmlwidgets/packwidget.js
@@ -38,7 +38,65 @@ HTMLWidgets.widget({
     
     var color = d3.scale.category10()
      .domain(d3.range(m));
-    
+   
+	var range = function(start, end, step) {
+	     var range = [];
+	     var typeofStart = typeof start;
+	     var typeofEnd = typeof end;
+
+	     if (step === 0) {
+	         throw TypeError("Step cannot be zero.");
+	     }
+
+	     if (typeofStart == "undefined" || typeofEnd == "undefined") {
+	         throw TypeError("Must pass start and end arguments.");
+	     } else if (typeofStart != typeofEnd) {
+	         throw TypeError("Start and end arguments must be of same type.");
+	     }
+
+	     typeof step == "undefined" && (step = 1);
+
+	     if (end < start) {
+	         step = -step;
+	     }
+
+	     if (typeofStart == "number") {
+
+	         while (step > 0 ? end >= start : end <= start) {
+	             range.push(start);
+	             start += step;
+	         }
+
+	     } else if (typeofStart == "string") {
+
+	         if (start.length != 1 || end.length != 1) {
+	             throw TypeError("Only strings with one character are supported.");
+	         }
+
+	         start = start.charCodeAt(0);
+	         end = end.charCodeAt(0);
+
+	         while (step > 0 ? end >= start : end <= start) {
+	             range.push(String.fromCharCode(start));
+	             start += step;
+	         }
+
+	     } else {
+	         throw TypeError("Only string and number types are supported");
+	     }
+
+	     return range;
+
+	 };
+	
+	 var ggcolor = function(i,n){
+		 var hues = range(15,375,360/(n-1));
+		 var hcl = hues.map(function(h){
+			 return d3.hcl(h,100,65);
+		 });
+		 return hcl[i];
+	 };
+	 
     clusters = new Array(m);
     nodes = xnode.map(function(e) {
         var i = e.cluster,
@@ -70,7 +128,7 @@ HTMLWidgets.widget({
     var node = svg.selectAll("circle")
      .data(nodes)
      .enter().append("circle")
-     .style("fill", function(d) { return color(d.cluster); })
+     .style("fill", function(d) { return ggcolor(d.cluster,6); })
      .call(force.drag);
     
     node.transition()
